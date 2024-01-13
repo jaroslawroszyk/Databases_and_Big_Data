2267c9f95a0cc8005d0347453ce35a910e629455
Jaroslaw Roszyk <roszyk.jarek@gmail.com>
2024-01-13 16:56:16 +0100

    solve task2

--- MapReduce/MapReduceRatingsMovies/MapReduce.py
+++ MapReduce/MapReduceRatingsMovies/MapReduce.py
@@ -1,52 +1,42 @@
 from mrjob.job import MRJob
-from mrjob.step import MRStep
+from mrjob.protocol import TextProtocol
 
-class MRMovieRatings(MRJob):
+class MRRatingAverage(MRJob):
 
-    def steps(self):
-        return [
-            MRStep(mapper=self.mapper_get_ratings,
-                   reducer=self.reducer_count_ratings),
-            MRStep(mapper=self.mapper_get_titles,
-                   mapper_init=self.mapper_init,
-                   reducer=self.reducer_add_titles)
-        ]
+    OUTPUT_PROTOCOL = TextProtocol
 
-    def mapper_get_ratings(self, _, line):
-        # Split the line from ratingsWith3.csv
-        (userId, movieId, rating, timestamp) = line.split(",")
-        try:
-            yield movieId, float(rating)
-        except ValueError:
-            # Skip lines with parsing errors
-            pass
-
-    def reducer_count_ratings(self, movieId, ratings):
-        total_ratings = sum(ratings)
-        num_ratings = len(ratings)
-        average_rating = total_ratings / num_ratings
-        # Emit movieId and average rating
-        yield movieId, (average_rating, num_ratings)
+    def configure_args(self):
+        super(MRRatingAverage, self).configure_args()
+        self.add_file_arg('--movies', help='path to movies.csv')
 
     def mapper_init(self):
-        # Load movies data
-        self.movie_titles = {}
+        self.movie_data = {}
+        with open(self.options.movies, 'r', encoding='utf-8') as movies_file:
+            for line in movies_file:
+                fields = line.strip().split(',')
+                if len(fields) >= 2:
+                    movie_id = fields[0]
+                    movie_title = fields[1]
+                    self.movie_data[movie_id] = movie_title
 
-        with open("moviesWith3.csv", "r") as f:
-            for line in f:
-                if line.strip() != '':
-                    parts = line.split(",")
-                    movieId = parts[0].strip()
-                    title = parts[1].strip()
-                    self.movie_titles[movieId] = title
+    def mapper(self, _, line):
+        if line.startswith("userId"):
+            return
 
-    def mapper_get_titles(self, movieId, movie_info):
-        # Emit movie title with the rating info
-        yield self.movie_titles.get(movieId), movie_info
+        fields = line.split(',')
+        movie_id = fields[1]
+        rating = float(fields[2])
 
-    def reducer_add_titles(self, title, movie_infos):
-        for movie_info in movie_infos:
-            yield title, movie_info
+        if movie_id in self.movie_data:
+            yield self.movie_data[movie_id], rating
+
+    def reducer(self, movie_title, ratings):
+        ratings_list = list(ratings)
+        total_ratings = len(ratings_list)
+        if total_ratings > 0:
+            average_rating = sum(ratings_list) / total_ratings
+            average_rating = round(sum(ratings_list) / total_ratings, 2)
+            yield None, f'{movie_title}: {average_rating}'
 
 if __name__ == '__main__':
-    MRMovieRatings.run()
+    MRRatingAverage.run()
